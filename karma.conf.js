6cdffe9ecb8cdda30b6338a88d9d508bb8ef3648
Fumio SAGAWA <fumio.sagawa@gmail.com>
2018-01-13 17:12:09 +0900

    add test, tslint

--- karma.conf.js
+++ karma.conf.js
@@ -1,25 +1,8 @@
-const webpackConfig = require('./config/webpack/test.js')
-module.exports = function(config) {
-  config.set({
-    basePath: "",
-    frameworks: ["jquery-3.2.1", "jasmine-jquery", "jasmine"],
-    plugins: [
-      "karma-jquery",
-      "karma-jasmine-jquery",
-      "karma-jasmine",
-      "karma-webpack",
-      "karma-chrome-launcher"
-    ],
-    files: [ "/* add spec files */" ],
-    exclude: [],
-    webpack: webpackConfig,
-    preprocessors: {"/* add spec files */" : ["webpack"]},
-    mime: { "text/x-typescript": ["ts"] },
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ["Chrome"],
-    singleRun: true
-  });
-};
+/**
+ * @author: @AngularClass
+ */
+
+/**
+ * Look in ./config for karma.conf.js
+ */
+module.exports = require('./config/karma.conf.js');
